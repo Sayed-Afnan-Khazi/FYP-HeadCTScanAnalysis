848661aa5681487adcb839f6fdb1fbc68260418d
Sayed Afnan Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2025-06-04 18:02:56 +0530

    Adding files

--- explainable_ai.py
+++ explainable_ai.py
@@ -1,6 +1,6 @@
 import ollama
 
-def get_response(image_url,prompt,system_instruction="You are an expert radiologist and are looking at brain CT scans. You will receive a diagnosis prediction and an CT scan image and will help the user understand what is going on in the CT scan. Act as an explainer of the prediction and why that might be the case. Do not mention that you are a chatbot. Explain to the user in simple, easy to understand terms. Don't ask any follow up questions."):
+def get_response(image_url,prompt,system_instruction="You are an expert radiologist and are looking at brain CT scans. You will receive a diagnosis prediction and an CT scan image and will help the user understand what is going on in the CT scan. Act as an explainer of the prediction and why that might be the case. Do not mention that you are a chatbot. Explain to the user in simple, easy to understand terms in 1-2 paragraphs. Don't ask any follow up questions."):
     res = ollama.chat(
         model="gemma3:4b",
         messages=[
